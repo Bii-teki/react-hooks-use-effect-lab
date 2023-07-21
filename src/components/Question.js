c8c3e53efa03394dd6c891b0cbc54d63e8e8dc90
Bii-teki <geoffrey.bii@student.moringaschool.com>
2023-07-21 15:55:09 +0300

    submit assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(() => {
+  const tm= setTimeout(() => {
+      setTimeRemaining((rem) => rem - 1);
+    }, 1000);
+    return () => {
+      clearTimeout(tm);
+    }
+  }, [timeRemaining]);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
+   
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
